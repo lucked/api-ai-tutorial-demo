00b21308d8c79a4b07a534e6cc1f9fe1d89a09a9
maximabramchuck <maximabramchuck@gmail.com>
2017-02-16 22:20:00 +0300

    Initial commit

---
+++ helpers/processMessage.js
@@ -0,0 +1,55 @@
+const API_AI_TOKEN = '<place your api ai token here>';
+const FACEBOOK_ACCESS_TOKEN = '<place your facebook page token here>';
+
+const request = require('request');
+
+const apiAiClient = require('apiai')(API_AI_TOKEN);
+
+const sendImage = (senderId, imageUri) => {
+    return request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: senderId },
+            message: {
+                attachment: {
+                    type: 'image',
+                    payload: { url: imageUri }
+                }
+            }
+        }
+    });
+};
+
+const sendTextMessage = (senderId, text) => {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: senderId },
+            message: { text },
+        }
+    });
+};
+
+module.exports = (event) => {
+    const senderId = event.sender.id;
+    const message = event.message.text;
+
+    const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'botcube_co'});
+
+    apiaiSession.on('response', (response) => {
+        const result = response.result.fulfillment.speech;
+
+        if (response.result.metadata.intentName === 'images.search') {
+            sendImage(senderId, result);
+        } else {
+            sendTextMessage(senderId, result);
+        }
+    });
+
+    apiaiSession.on('error', error => console.log(error));
+    apiaiSession.end();
+};
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const verificationController = require('./controllers/verification');
+const messageWebhookController = require('./controllers/messageWebhook');
+const imageSearchController = require('./controllers/imageSearch');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('Webhook server is listening at port 5000'));
+
+app.get('/', verificationController);
+app.post('/', messageWebhookController);
+app.post('/image-search', imageSearchController);
